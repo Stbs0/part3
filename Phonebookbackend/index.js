904881c846b5174620e464bd586abc810e863bfd
Stbs0 <mohammedjrt@gmail.com>
2024-05-29 07:07:26 +0300

    please work

--- Phonebookbackend/index.js
+++ Phonebookbackend/index.js
@@ -3,7 +3,7 @@ var morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(express.static("dist"));
+
 app.use(cors());
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -81,7 +81,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
