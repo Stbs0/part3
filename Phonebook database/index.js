00c724004070dd46298a08632238c918a3511f6a
Stbs0 <mohammedjrt@gmail.com>
2024-05-30 05:30:40 +0300

    finish exercie 3.18

--- Phonebook database/index.js
+++ Phonebook database/index.js
@@ -7,11 +7,12 @@ const Contact = require("./modules/phonebook");
 let phonebook = [];
 
 app.use(express.static("dist"));
+app.use(express.json());
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
+  console.log("Method:", request.method, response.method);
+  console.log("Path:  ", request.path, response.path);
+  console.log("Body:  ", request.body, response.body);
   console.log("---");
   next();
 };
@@ -20,46 +21,67 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use(express.json());
 app.use(requestLogger);
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res,next) => {
   Contact.find({}).then((result) => {
     res.json(result);
-  });
+  }).catch(error=>next(error));
 });
 
-app.get("/info", (req, res) => {
-  Contact.find({}).then((result) => {
-    res.send(
-      `<p> Phonebook has info for ${
-        result.length
-      } people </p> <br/> <p> ${new Date()}</p>`
-    );
-  });
+app.get("/info", (req, res, next) => {
+  Contact.find({})
+    .then((result) => {
+      res.send(
+        `<p> Phonebook has info for ${
+          result.length
+        } people </p> <br/> <p> ${new Date()}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  const person = phoneBook.filter((n) => Number(n.id) === id);
-  if (person.length === 0) {
-    res.status(404).end("Error: not found");
-  }
-
-  res.json(person);
+app.get("/api/persons/:id", (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then((contact) => {
+      if (contact) {
+        res.json(contact);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+      res.status(500).end();
+    });
 });
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  phoneBook = phoneBook.filter((n) => n.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Contact.findByIdAndDelete(req.params.id)
+    .then((deleted) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-
-app.post("/api/persons", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  const contact = {
+    name: body.name,
+    number: body.number,
+  };
+  console.log(req.params.id);
+  Contact.findByIdAndUpdate(req.params.id, contact, { new: true })
+    .then((updated) => {
+      console.log(updated);
+      res.json(updated);
+    })
+    .catch((error) => next(error));
+});
+app.post("/api/persons", (req, res,next) => {
   const body = req.body;
   if (body === undefined) {
     return res.status(405).end("please fill right");
@@ -73,7 +95,17 @@ app.post("/api/persons", (req, res) => {
   console.log(person);
   person.save().then((result) => {
     res.json(result);
-  });
+  }).catch(error=>next(error));
+});
+app.use(unknownEndpoint);
+
+app.use((error, req, res, next) => {
+  if (error.name === "CastError") {
+    console.log(error.message);
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
 });
 
 const PORT = process.env.PORT;
